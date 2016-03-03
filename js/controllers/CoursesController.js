fcd49e1b9b21143bf3997c49ca4af58596def880
Robison <robison@Robisons-MacBook-Pro.local>
2016-03-03 07:56:47 -0700

    adding in a few styles.

--- js/controllers/CoursesController.js
+++ js/controllers/CoursesController.js
@@ -4,34 +4,62 @@ coursesApp.controller('CoursesController',
     $scope.pageURL = 'http://canvas-api.herokuapp.com/api/v1/courses';
 
     // Lets get the token so we can use that to get course data.
-    authToken.getToken()
-      .success(function(event) {
-        $scope.token = event.token;
-      })
-      .error(function(data) {
-        console.log('could not find token');
-      })
-      .then(function(response) {
+    getCourseData = function() {
 
-        coursesData.getCourses($scope.pageURL, $scope.token)
-          .success(function(data, status, headers, config) {
+      authToken.getToken()
+        .success(function(event) {
+          $scope.token = event.token;
+        })
+        .error(function(data) {
+          console.log('could not find token');
+        })
+        .then(function(response) {
 
-            if ( angular.isArray(data) ) {
-              $scope.courses = data;
-              
-            } else {
-              $scope.courses = false;
-              $scope.course = data
-            }
+          coursesData.getCourses($scope.pageURL, $scope.token)
+            .success(function(data, status, headers, config) {
 
-          })
-          .error(function(data, status, headers, config) {
+              if ( headers('Link') !== null ) {
+                parseLinks( headers('Link').split(',') );
+              }
 
-            console.log('could not find courses');
+              if ( angular.isArray(data) ) {
+                $scope.courses = data;
+                
+              } else {
+                $scope.courses = false;
+                $scope.course = data
+              }
 
-          });
+            })
+            .error(function(data, status, headers, config) {
 
-      });
+              console.log('could not find courses');
+
+            });
+
+        });
+      
+    }
+
+      parseLinks = function(links) {
+
+        $scope.pagination = {};
+        
+        angular.forEach(links, function (l) {
+          
+          var linkParts = l.split(';'),
+            rel = linkParts[1].replace(/rel="(.*)"/, '$1').trim(),
+            url = linkParts[0].replace(/<(.*)>/, '$1').trim();
+
+          if ( rel == 'prev' || rel == 'next' ) {
+            $scope.pagination[rel] = url;
+          }
+
+        });
+
+      }
+
+      getCourseData();
 
 	}
 );
